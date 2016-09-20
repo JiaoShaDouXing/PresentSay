8e4cbb449e309879896ad62767e801d62547ddb8
FakeIsTrue <18210459036@163.com>
2016-09-20 11:17:43 +0800

    init

--- app/src/main/java/com/wtf/secondproject/ui/BaseFragment.java
+++ app/src/main/java/com/wtf/secondproject/ui/BaseFragment.java
@@ -1,11 +1,7 @@
 package com.wtf.secondproject.ui;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 /**
  * Created by 宋博通 on 2016/9/20.
